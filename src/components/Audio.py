05ae9db6ccbd100f1c01e5346704733ca4b10f6e
neszha <fanesahadi@gmail.com>
2022-05-21 16:24:30 +0700

    Kontrol volume audio

--- src/components/Audio.py
+++ src/components/Audio.py
@@ -3,10 +3,11 @@ import src.state as state
 
 class Audio:
 
-    def __init__(self, path, audio_type='sound_fx'):
+    def __init__(self, path, audio_type='sound_fx', volume=0.5):
         self.__type = audio_type # (music, sound_fx)
         self.__audio = pg.mixer.Sound(path)
         self.__temp_enable = False
+        self.__volume = volume
         self.watch_setting()
 
     def set_volume(self, volume=0.5):
@@ -19,18 +20,16 @@ class Audio:
     def watch_setting(self):
         volume = 0
         if self.__type == 'sound_fx' and self.__temp_enable != state.SOUND_FX:
-            if state.SOUND_FX: volume = 0.5
+            if state.SOUND_FX: volume = self.__volume
             self.set_volume(volume)
             self.__temp_enable = state.SOUND_FX
-            print('change volume sound fx')
         elif self.__type == 'music' and self.__temp_enable != state.MUSIC:
-            if state.MUSIC: volume = 0.5
+            if state.MUSIC: volume = self.__volume
             self.set_volume(volume)
             self.__temp_enable = state.MUSIC
-            print('change volume music')
 
     def play(self, loops=False):
-        self.set_volume()
+        self.set_volume(self.__volume)
         self.__audio.play(loops)
         
     def stop(self):
